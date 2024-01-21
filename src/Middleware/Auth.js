f5e68aa7942d6386f43b39c7076cebb6f7ac7d3e
MuGrahiman <mujeebrahiman2000@gmail.com>
2024-01-21 08:22:52 +0530

    feat:create middleware for protect the route

---
+++ src/Middleware/Auth.js
@@ -0,0 +1,21 @@
+import Auth from "../Helpers/Auth.js";
+const auth = async (req, res, next) => {
+	try {
+		const { authorization } = req.headers;
+		if (!authorization || !authorization.startsWith("Bearer"))
+			throw new Error("Authorization required");
+
+		const token = authorization.split(" ")[1];
+		if (!token) throw new Error("token required");
+
+		const decodeData = await Auth.CheckToken(token);
+		if (!decodeData) throw new Error("Not an valid Token");
+
+		req.userId = decodeData?._id;
+		next();
+	} catch (error) {
+		console.log(error);
+		res.status(400).json("Invalid Credentials");
+	}
+};
+export default auth;
